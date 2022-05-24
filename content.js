c0d93d01abb8a3651a2f7a103238f4816e32d8e3
David Nguyen <david.nguyen@columbia.edu>
2022-05-24 19:06:21 -0400

    updated to MV3, cleaned up code, removed useless jquery

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-chrome.runtime.onMessage.addListener(function (request) {
-    var first = new RegExp(request[0], "gi");
-    var second = request[1];
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+    let first = msg.msg[0];
+    let second = msg.msg[1];
+    let regex = new RegExp(first, "gi");
 
-    $('body :not(script)').contents().filter(function () {
-        return this.nodeType === 3;
-    }).replaceWith(function () {
-        return this.nodeValue.replace(first, second);
-    });
+    let nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
+    let currentNode;
+    while (currentNode = nodeIterator.nextNode()) {
+        currentNode.textContent = currentNode.textContent.replace(regex, second);
+    }
 
+    response('success!');
 })
--- popup.js
+++ popup.js
@@ -1,13 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
     
-    document.querySelector('button').addEventListener('click', onclick, false)
+//     document.querySelector('button').addEventListener('click', onclick, false)
 
-    function onclick () {
-        var first = document.getElementById("first").value;
-        var second = document.getElementById("second").value;
+//     function onclick () {
+//         var first = document.getElementById("first").value;
+//         var second = document.getElementById("second").value;
+//         chrome.tabs.query({currentWindow: true, active: true},
+//         function (tabs) {
+//             chrome.tabs.sendMessage(tabs[0].id, [first, second])
+//         })
+//     }
+// }, false)
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('#replace-btn');
+    const first = document.querySelector('#first');
+    const second = document.querySelector('#second');
+    let firstmsg;
+    let secondmsg;
+
+    btn.addEventListener('click', () => {
+        firstmsg = first.value;
+        secondmsg = second.value;
         chrome.tabs.query({currentWindow: true, active: true},
         function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, [first, second])
-        })
-    }
-}, false)
+            chrome.tabs.sendMessage(tabs[0].id, {msg: [firstmsg, secondmsg]});
+        });
+    });
+});
