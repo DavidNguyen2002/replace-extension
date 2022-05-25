faee3ac0f68e358eb171c1d0c48bb685f300bc44
David Nguyen <david.nguyen@columbia.edu>
2022-05-24 21:17:38 -0400

    Fixed comments

--- popup.js
+++ popup.js
@@ -1,19 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-    
-//     document.querySelector('button').addEventListener('click', onclick, false)
-
-//     function onclick () {
-//         var first = document.getElementById("first").value;
-//         var second = document.getElementById("second").value;
-//         chrome.tabs.query({currentWindow: true, active: true},
-//         function (tabs) {
-//             chrome.tabs.sendMessage(tabs[0].id, [first, second])
-//         })
-//     }
-// }, false)
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector('#replace-btn');
     const first = document.querySelector('#first');
